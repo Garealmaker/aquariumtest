803766acaaaea7d8f58f6971da2c94e7ec41aed2
Garealmaker <lescieux.cyril@gmail.com>
2026-03-17 15:53:12 +0100

    Delete Online version directory

--- Online version/supabase-config.example.js
+++
@@ -1,4 +0,0 @@
-window.AQUARIOW_SUPABASE = {
-  url: "https://YOUR_PROJECT_ID.supabase.co",
-  anonKey: "YOUR_SUPABASE_ANON_KEY",
-};
--- Online version/supabase-config.js
+++
@@ -1,4 +0,0 @@
-window.AQUARIOW_SUPABASE = {
-  url: "https://dfbjijhukgolomjwqoct.supabase.co",
-  anonKey: "sb_publishable_Bn9PJ6cdl0N5Bwr3Obe9fg_uL5_EJyU",
-};
